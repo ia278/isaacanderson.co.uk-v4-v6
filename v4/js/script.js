5c4aa693aab64cd3399f556752d805d0926bd9d9
Isaac Anderson <isaacaanderson4@gmail.com>
2018-10-14 13:13:59 +0100

    add new header design

--- v4/js/script.js
+++ v4/js/script.js
@@ -1,45 +1,15 @@
-// Constants
-const article = document.querySelector("article");
-const footer = document.querySelector("footer");
-const hamburger = document.querySelector(".hamburger");
-const options = document.querySelector(".header-options");
-const section = document.querySelectorAll("section");
+let prevScrollpos = window.pageYOffset;
 
-// Functions
-const actionOnWindowResize = () => {
-	const windowWidth = window.innerWidth;
+window.onscroll = function() {
+  const windowWidth = window.innerWidth;
 	if (windowWidth >= 768) {
-		options.style.display = "block";
 		return;
-	}
-	options.style.display = "none";
-	hamburger.classList.remove("change");
-};
-const animateHamburger = () => {
-	hamburger.classList.toggle("change");
-	if (hamburger.classList.contains("change")) {
-		options.style.display = "block";
-    for (let i = 0; i < section.length; i++) {
-      section[i].addEventListener("click", noMenuDisplay);
-    } 
-    article.addEventListener("click", noMenuDisplay);
-    footer.addEventListener("click", noMenuDisplay);
-		return;
-	}
-	options.style.display = "none";
-	actionOnWindowResize();
-};
-
-const noMenuDisplay = () => {
-  hamburger.classList.toggle("change");
-	options.style.display = "none";
-  for (let i = 0; i < section.length; i++) {
-    section[i].removeEventListener("click", noMenuDisplay);
+	} 
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".header").style.top = "0";
+  } else {
+    document.querySelector(".header").style.top = "-250px";
   }
-  article.removeEventListener("click", noMenuDisplay);
-  footer.removeEventListener("click", noMenuDisplay);
-};
-
-// Event listeners
-hamburger.addEventListener("click", animateHamburger);
-window.addEventListener("resize", actionOnWindowResize);
+  prevScrollpos = currentScrollPos;
+}
