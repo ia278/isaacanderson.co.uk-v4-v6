5e2fe98340b24cf478821af32b5e35c4b8fa9502
Isaac Anderson <isaacaanderson4@gmail.com>
2018-10-14 17:20:30 +0100

    change mobile header positioning to relative

--- v4/js/script.js
+++ v4/js/script.js
@@ -1,15 +1,15 @@
-let prevScrollpos = window.pageYOffset;
-
-window.onscroll = function() {
-  const windowWidth = window.innerWidth;
-	if (windowWidth >= 768) {
-		return;
-	} 
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.querySelector(".header").style.top = "0";
-  } else {
-    document.querySelector(".header").style.top = "-250px";
-  }
-  prevScrollpos = currentScrollPos;
-}
+//let prevScrollpos = window.pageYOffset;
+//
+//window.onscroll = function() {
+//  const windowWidth = window.innerWidth;
+//	if (windowWidth >= 768) {
+//		return;
+//	} 
+//  let currentScrollPos = window.pageYOffset;
+//  if (prevScrollpos > currentScrollPos) {
+//    document.querySelector(".header").style.top = "0";
+//  } else {
+//    document.querySelector(".header").style.top = "-250px";
+//  }
+//  prevScrollpos = currentScrollPos;
+//}
