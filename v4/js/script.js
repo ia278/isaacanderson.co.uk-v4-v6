8e14e173b36e72a904d7583d8b3709a47b1a1c69
Isaac Anderson <isaacaanderson4@gmail.com>
2018-11-08 14:16:17 +0000

    delete header scroll styles

--- v4/js/script.js
+++ v4/js/script.js
@@ -1,7 +1,7 @@
 // Constants
-const br = document.querySelectorAll(".related-contents br")
+const br = document.querySelectorAll(".related-contents br");
+const introInArticle = document.querySelector(".article-contents p");
 const linksInArticle = document.querySelectorAll(".article-contents a");
-let prevScrollpos = window.pageYOffset;
 
 
 // Functions
@@ -21,23 +21,3 @@ const removeBr = () => {
 // Invoke function
 addTargetAttribute();
 removeBr();
-
-
-
-
-
-//const showMenuOnScroll = () => {
-//  const windowWidth = window.innerWidth;
-//	if (windowWidth >= 768) {
-//		return;
-//	} 
-//  let currentScrollPos = window.pageYOffset;
-//  if (prevScrollpos > currentScrollPos) {
-//    document.querySelector(".header").style.top = "0";
-//  } else {
-//    document.querySelector(".header").style.top = "-250px";
-//  }
-//  prevScrollpos = currentScrollPos;
-//}
-//
-//window.addEventListener("scroll", showMenuOnScroll);
