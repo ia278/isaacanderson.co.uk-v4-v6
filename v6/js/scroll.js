c92971ec17d5c2fcbcd48b4838e1458565b3d099
Isaac Anderson <isaacaanderson4@gmail.com>
2019-01-22 12:25:20 +0000

    add pseudoelement to top of active section

--- v6/js/scroll.js
+++ v6/js/scroll.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($){
 	
 	// Add smooth scrolling to all links
 	$("a").on('click', function(event) {
-
+		
 		// Make sure this.hash has a value before overriding default behavior
 		if (this.hash !== "") {
 			// Prevent default anchor click behavior
@@ -22,5 +22,7 @@ jQuery(document).ready(function($){
 			});
 		} // End if
 	});
+	
+	
 	
 });
