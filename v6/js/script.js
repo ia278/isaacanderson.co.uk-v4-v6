ff530d19d4f9f1800c00d2f84e361082b19d31a7
Isaac Anderson <isaacaanderson4@gmail.com>
2019-01-29 18:42:41 +0000

    add alignment div via js

--- v6/js/script.js
+++ v6/js/script.js
@@ -1,11 +1,40 @@
 // Constants
+const archiveCard = document.querySelectorAll(".archive-listing .archive-card");
 const articleNav = document.querySelector(".article-nav");
 const br = document.querySelectorAll(".related-contents br");
 const h2InArticle = document.querySelectorAll(".article h2");
 const linksInArticle = document.querySelectorAll(".article-contents a");
+const postsCard = document.querySelectorAll(".posts-listing .posts-card");
 
 
 // Functions
+const addArchiveAlignmentCard = () => {
+	if (archiveCard.length === 0) {
+		return;
+	}
+	// Attach first alignment div
+	const div = document.createElement("DIV");
+	div.setAttribute("class", `archive-card archive-card-align`);
+	const lastCard = archiveCard[archiveCard.length - 1];
+	lastCard.parentNode.insertBefore(div, lastCard.nextSibling);
+};
+const addPostAlignmentCard = () => {
+	if (postsCard.length === 0) {
+		return;
+	}
+	// Attach first alignment div
+	const div = document.createElement("DIV");
+	div.setAttribute("class", `posts-card posts-card-align`);
+	const lastCard = postsCard[postsCard.length - 1];
+	lastCard.parentNode.insertBefore(div, lastCard.nextSibling);
+	
+	// Attach second alignment div
+	const div2 = document.createElement("DIV");
+	div2.setAttribute("class", `posts-card posts-card-align`);
+	const updatedPostsCard = document.querySelectorAll(".posts-listing .posts-card");
+	const updatedLastCard = updatedPostsCard[updatedPostsCard.length - 1];
+	updatedLastCard.parentNode.insertBefore(div2, updatedLastCard.nextSibling);
+};
 const addTargetAttribute = () => {
 	if (linksInArticle.length === 0) {
 		return;
@@ -21,8 +50,6 @@ const createHeaderList = () => {
 	const ol = document.createElement("OL");
 	for (let i = 0; i < h2InArticle.length; i++) {
 		// Create ID, add to headers in article
-//		const idText = h2InArticle[i].innerHTML.split(" ").join("-");
-//		const id = idText.toLowerCase();
 		const id = `ia${i}`;
 		h2InArticle[i].setAttribute("id", id);
 		
@@ -49,7 +76,10 @@ const removeBr = () => {
 	}
 };
 
+
 // Invoke functions
+addArchiveAlignmentCard();
+addPostAlignmentCard();
 addTargetAttribute();
 createHeaderList();
 removeBr();
